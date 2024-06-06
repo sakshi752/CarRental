da9ca28ea426bf44e5d6873de13dcc6563452387
Sakshi <saks0705@gmail.com>
2024-06-06 22:23:27 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { Outlet } from "react-router-dom"
-import Header from "./Components/Header"
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
-      <div className="bg-gray-100 min-h-screen">
-        <Header />
-        <Outlet/>
-        <img className="h-96" src="public/red-sports-car-png-1.png" alt="" />
-      </div>
+      <Scrollbars style={{ height: '100vh',width:"100%" }}>
+        <div className="bg-gray-100 min-h-screen sm:px-5 md:px-10 lg:px-16 py-4">
+          <Header />
+          <Outlet />
+        </div>
+      </Scrollbars>
     </>
   )
 }
 
-export default App
+export default App;
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FaCarSide, FaEarthAfrica } from "react-icons/fa6";
+import { FaRegClock } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/blogs"}>Blogs</Link>
-      <Link to={"/cars"}>Cars</Link>
-      <Link to={"/contact"}>Contact</Link>
+    <div className='flex flex-col'>
+      {/* section1 */}
+      <div className='flex items-center justify-between'>
+        <p>Need help?8031284120</p>
+        <div>
+          <button>login</button>
+          <button>Register</button>
+        </div>
+      </div>
+
+      {/* section2 */}
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center'>
+          <FaCarSide className='text-red-500 text-4xl' />
+          CarRental
+        </div>
+        <div className='flex items-center'>
+          <FaEarthAfrica />
+          <h1>Vadodara,Gujrat</h1>
+        </div>
+        <div className='flex items-center'>
+          <FaRegClock />
+          monday-sunday
+        </div>
+      </div>
+
+      {/* section3 */}
+      <div className='flex items-center justify-between'>
+        <div>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/blogs"}>Blogs</Link>
+          <Link to={"/cars"}>Cars</Link>
+          <Link to={"/contact"}>Contact</Link>
+        </div>
+        <div>
+          <input type="search" />
+        </div>
+      </div>
     </div>
+
   )
 }
 
