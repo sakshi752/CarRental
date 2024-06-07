a7eab046e46fced6dc61b2957f001b756cfb8412
Sakshi <saks0705@gmail.com>
2024-06-07 16:49:02 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <>
-      <Scrollbars style={{ height: '100vh',width:"100%" }}>
-        <div className="bg-gray-100 min-h-screen sm:px-5 md:px-10 lg:px-16 py-4">
+      <Scrollbars style={{ height: '100vh', width: "100%" }}>
+        <div className='bg-gray-100 min-h-screen tracking-widest'>
           <Header />
-          <Outlet />
+          <div className=" px-3 sm:px-5 md:px-10 lg:px-16 py-4">
+            <Outlet />
+          </div>
+          <Footer />
         </div>
+
       </Scrollbars>
     </>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,46 +5,42 @@ import { FaRegClock } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className='flex flex-col'>
-      {/* section1 */}
-      <div className='flex items-center justify-between'>
-        <p>Need help?8031284120</p>
-        <div>
-          <button>login</button>
-          <button>Register</button>
+    <div>
+      <div className='flex items-center justify-between py-3 px-2 sm:px-3 md:px-7 '>
+        <div className='flex items-center gap-3 cursor-pointer'>
+          <FaCarSide className='text-red-500 text-[3.2rem]' />
+          <div>
+            <h1 className='text-2xl font-bold'>Car</h1>
+            <p className='text-md font-semibold'>Rental</p>
+          </div>
+        </div>
+        <div className='flex space-x-3 text-md font-semibold'>
+          <Link className='hover:text-red-600 hover:underline' to={"/"}>Home</Link>
+          <Link className='hover:text-red-600 hover:underline' to={"/about"}>About</Link>
+          <Link className='hover:text-red-600 hover:underline' to={"/blogs"}>Blogs</Link>
+          <Link className='hover:text-red-600 hover:underline' to={"/cars"}>Cars</Link>
+          <Link className='hover:text-red-600 hover:underline' to={"/contact"}>Contact</Link>
+        </div>
+        <div className='flex gap-4'>
+          <button className='bg-red-500 px-2 py-3 rounded text-white font-semibold hover:bg-red-600 shadow-lg shadow-red-200'>Login</button>
+          <button className='bg-red-500 px-2 py-3 rounded text-white font-semibold hover:bg-red-600 shadow-lg shadow-red-200'>Register</button>
         </div>
       </div>
-
-      {/* section2 */}
-      <div className='flex items-center justify-between'>
-        <div className='flex items-center'>
-          <FaCarSide className='text-red-500 text-4xl' />
-          CarRental
+      <div className='flex items-center justify-between bg-red-500 text-white py-1 px-2 sm:px-3 md:px-7 '>
+        <div>
+          <h1>need help?9090909090</h1>
         </div>
-        <div className='flex items-center'>
+        {/* <div className='flex items-center'>
           <FaEarthAfrica />
           <h1>Vadodara,Gujrat</h1>
-        </div>
-        <div className='flex items-center'>
+        </div> */}
+        <div className='flex items-center gap-3'>
           <FaRegClock />
-          monday-sunday
-        </div>
-      </div>
-
-      {/* section3 */}
-      <div className='flex items-center justify-between'>
-        <div>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/about"}>About</Link>
-          <Link to={"/blogs"}>Blogs</Link>
-          <Link to={"/cars"}>Cars</Link>
-          <Link to={"/contact"}>Contact</Link>
-        </div>
-        <div>
-          <input type="search" />
+          <h1>Monday-Sunday</h1>
         </div>
       </div>
     </div>
+
 
   )
 }
